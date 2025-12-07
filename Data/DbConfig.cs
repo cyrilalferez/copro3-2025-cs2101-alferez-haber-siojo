f4650ee57de69b5584d115796ac89eaf71b244c0
kingczire <emanuelczire12@gmail.com>
2025-12-07 12:49:53 +0800

    100% Done

--- Data/DbConfig.cs
+++ Data/DbConfig.cs
@@ -2,7 +2,8 @@
 {
     internal class DbConfig
     {
-        public static readonly string DbPath = Path.Combine(Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName, "ZombieSurvival.db");
+        //public static readonly string DbPath = Path.Combine(Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName, "ZombieSurvival.db");
+        public static readonly string DbPath = "ZombieSurvival.db";
         public static readonly string ConnectionString = $"Data Source={DbPath}";
     }
 }
